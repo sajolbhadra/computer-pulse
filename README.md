4ae7bc0053da39f385fe4b5744d0e5c45a159f76
sajolbhadra <sajolbhadra@gmail.com>
2022-03-26 13:46:54 +0600

    project initiated

---
+++ README.md
@@ -0,0 +1,8 @@
+# Project Name: Shop BD
+
+# Live Site: https://shop-bd.netlify.app
+
+* Project Description:
+    
+    1. The site is about shopping PC Component.
+    2. User can choose Component on his/her desired from the items display in the site.
---
+++ public/fakedata.js
@@ -0,0 +1,110 @@
+const pcComponent = [
+    {
+        id:1,
+        category: 'MotherBoard',
+        name: 'MSI Tomahawk B550',
+        price: 15500,
+        img: 'https://storage-asset.msi.com/global/picture/image/feature/mb/B550/MAG/TOMAHAWK/msi-mag-b550-tomahawk-hero-board01.png',
+        rating: 5,
+        warrenty: '3 Years'
+    },
+    {
+        id:2,
+        category: 'MotherBoard',
+        name: 'GIGABAYTE AORUS B550',
+        price: 17500,
+        img: 'https://m.media-amazon.com/images/I/81jsvXXWg6L._AC_SX679_.jpg',
+        rating: 4.8,
+        warrenty: '3 Years'
+    },
+    {
+        id:3,
+        category: 'CPU',
+        name: 'Intel i5 12400',
+        price: 19500,
+        img: 'https://cdn.mos.cms.futurecdn.net/4cthTRg6CzcDpYAwusAo5c-970-80.jpg.webp',
+        rating: 5,
+        warrenty: '3 Years'
+    },
+    {
+        id:4,
+        category: 'CPU',
+        name: 'AMD Ryzen 5 3600',
+        price: 19600,
+        img: 'https://static.techspot.com/articles-info/1968/images/2020-01-08-image-3-j_1100.webp',
+        rating: 5,
+        warrenty: '3 Years'
+    },
+    {
+        id:5,
+        category: 'RAM',
+        name: 'G.Skill Trident Z 8GB DDR4',
+        price: 4200,
+        img: 'https://www.binarylogic.com.bd/images/product_image/mediumg-skill-trident-z-8gb%20ddr4-desktop-3200mhz-ram.webp',
+        rating: 5,
+        warrenty: 'lifetime'
+    },
+    {
+        id:6,
+        category: 'RAM',
+        name: 'Corsair Vengence LPX 8GB DDR4',
+        price: 3800,
+        img: 'https://www.startech.com.bd/image/cache/catalog/RAM/VENG_LPX_BLK_01-500x500.png',
+        rating: 4.9,
+        warrenty: 'lifetime'
+    },
+    {
+        id:7,
+        category: 'GPU',
+        name: 'NVidia RTX 3050 Ti',
+        price: 51000,
+        img: 'https://assets.mspimages.in/gear/wp-content/uploads/2020/11/Nvidia-RTX-3060Ti-696x365.jpg',
+        rating: 5,
+        warrenty: '3 Years'
+    },
+    {
+        id:8,
+        category: 'GPU',
+        name: 'AMD RX 6600 XT  ',
+        price: 68000,
+        img: 'https://techgage.com/wp-content/uploads/2021/08/PowerColor-Radeon-RX-6600-XT-Red-Devil-with-Packaging-680x578.jpg',
+        rating: 4.8,
+        warrenty: '3 Years'
+    },
+    {
+        id:9,
+        category: 'M.2 NVMe',
+        name: 'Samsung 970 EVO Plus 1TB',
+        price: 15500,
+        img: 'https://www.startech.com.bd/image/cache/catalog/ssd/samsung/970-evo-plus-1tb/970-evo-plus-1tb-500x500.jpg',
+        rating: 5,
+        warrenty: '5 Years'
+    },
+    {
+        id:10,
+        category: 'M.2 NVMe',
+        name: 'Corsair MPG5 1TB',
+        price: 17500,
+        img: 'https://computermaniabd.b-cdn.net/wp-content/uploads/Corsair-Force-MP510-240-GB-NVMe-PCIe-Gen3-M.2-SSD.jpg',
+        rating: 4.8,
+        warrenty: '3 Years'
+    },
+    {
+        id:11,
+        category: 'PSU',
+        name: 'Corsair CX 650 Watt',
+        price: 6000,
+        img: 'https://microless.com/cdn/products/c6dd95de8ce5f0e1a3ddf71bac60e1a8-md.jpg',
+        rating: 5,
+        warrenty: '5 Years'
+    },
+    {
+        id:12,
+        category: 'PSU',
+        name: 'Tharmaltake Litepower 650 Watt',
+        price: 6200,
+        img: 'https://www.thermaltake.com/pub/media/wysiwyg/key3/db/products/psu/ltp35455565/main_650.jpg',
+        rating: 4,
+        warrenty: '5 Years'
+    }
+]
